2257b61cda4e130f35ba3225f9f45cb175f9b33c
csaboss92 <csaboss92@gmail.com>
2017-10-03 13:54:15 +0200

    Renamed AppComponent to HeroComponent and added AppComponent.

--- Angular-hero/src/app/app.module.js
+++ Angular-hero/src/app/app.module.js
@@ -8,8 +8,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
-var app_component_1 = require("./app.component");
+var heroes_component_1 = require("./heroes.component");
 var hero_detail_component_1 = require("./hero-detail.component");
+var app_component_1 = require("./app.component");
+var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -20,8 +22,10 @@ AppModule = __decorate([
         imports: [platform_browser_1.BrowserModule,
             forms_1.FormsModule],
         declarations: [app_component_1.AppComponent,
-            hero_detail_component_1.HeroDetailComponent],
-        bootstrap: [app_component_1.AppComponent]
+            hero_detail_component_1.HeroDetailComponent,
+            heroes_component_1.HeroesComponent],
+        bootstrap: [app_component_1.AppComponent],
+        providers: [hero_service_1.HeroService]
     })
 ], AppModule);
 exports.AppModule = AppModule;
--- Angular-hero/src/app/app.module.ts
+++ Angular-hero/src/app/app.module.ts
@@ -2,15 +2,19 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
-import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
+import {AppComponent} from "./app.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
   imports: [BrowserModule,
     FormsModule],
   declarations: [AppComponent,
-    HeroDetailComponent],
-  bootstrap: [AppComponent]
+    HeroDetailComponent,
+    HeroesComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
 export class AppModule {
 }
