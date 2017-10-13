d8630bb8a705e9ade887d57b2749809d443bb0b6
csaboss92 <csaboss92@gmail.com>
2017-10-13 14:23:10 +0200

    Replaced the mock heroes list with an inmemory http service

--- Angular-hero/src/app/hero.service.ts
+++ Angular-hero/src/app/hero.service.ts
@@ -1,15 +1,27 @@
 import {Injectable} from '@angular/core';
 import {Hero} from "./hero";
-import {HEROES} from "./mock-heroes";
+import {Http} from "@angular/http";
+import "rxjs/add/operator/toPromise";
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    // return new Promise(resolve => {
-    //   setTimeout(() => resolve(HEROES), 2000)
-    // });
-    return Promise.resolve(HEROES);
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) {
   }
+
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error)
+    return Promise.reject(error.message || error);
+  }
+
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
       .then(heroes => heroes.find(hero => hero.id === id));
