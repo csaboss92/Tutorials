7a4e3a10c01e8bc11cb876e1a755e329946f75de
csaboss92 <csaboss92@gmail.com>
2017-10-13 14:03:32 +0200

    Added styles and refactored the templates, etc...

--- Angular-hero/src/app/dashboard.component.ts
+++ Angular-hero/src/app/dashboard.component.ts
@@ -4,14 +4,16 @@ import {HeroService} from "./hero.service";
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService)
-  {}
+  constructor(private heroService: HeroService) {
+  }
+
   ngOnInit(): void {
-    this.heroService.getHeroes().then(heroes=>this.heroes = heroes.slice(0,4))
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(0, 4))
   }
 }
