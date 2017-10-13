a5223f1930791a3487d12ad13589b01b9feba1ed
csaboss92 <csaboss92@gmail.com>
2017-10-13 10:57:14 +0200

    Added dashboard with routing

--- Angular-hero/src/app/app.module.js
+++ Angular-hero/src/app/app.module.js
@@ -8,11 +8,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
+var router_1 = require("@angular/router");
 var heroes_component_1 = require("./heroes.component");
 var hero_detail_component_1 = require("./hero-detail.component");
 var app_component_1 = require("./app.component");
 var hero_service_1 = require("./hero.service");
-var router_1 = require("@angular/router");
+var dashboard_component_1 = require("./dashboard.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -24,12 +25,20 @@ AppModule = __decorate([
             platform_browser_1.BrowserModule,
             forms_1.FormsModule,
             router_1.RouterModule.forRoot([{
+                    path: '',
+                    redirectTo: '/dashboard',
+                    pathMatch: 'full'
+                }, {
                     path: 'heroes',
                     component: heroes_component_1.HeroesComponent
+                }, {
+                    path: 'dashboard',
+                    component: dashboard_component_1.DashboardComponent
                 }])
         ],
         declarations: [
             app_component_1.AppComponent,
+            dashboard_component_1.DashboardComponent,
             hero_detail_component_1.HeroDetailComponent,
             heroes_component_1.HeroesComponent
         ],
--- Angular-hero/src/app/app.module.ts
+++ Angular-hero/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from "@angular/router";
+
 
 import {HeroesComponent} from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {AppComponent} from "./app.component";
 import {HeroService} from "./hero.service";
-import {RouterModule} from "@angular/router";
+import {DashboardComponent} from "./dashboard.component";
 
 
 
@@ -15,12 +17,20 @@ import {RouterModule} from "@angular/router";
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([{
+      path:'',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    },{
       path: 'heroes',
       component: HeroesComponent
+    },{
+      path: 'dashboard',
+      component: DashboardComponent
     }])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
