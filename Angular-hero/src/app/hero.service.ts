b02a16491ba2a6095eb60912eef11b2097f11fc7
csaboss92 <csaboss92@gmail.com>
2017-10-13 15:20:41 +0200

    Ability to add and delete heroes

--- Angular-hero/src/app/hero.service.ts
+++ Angular-hero/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Hero} from "./hero";
-import {Http, Headers} from "@angular/http";
+import {Headers, Http} from "@angular/http";
 import "rxjs/add/operator/toPromise";
 
 @Injectable()
@@ -23,19 +23,35 @@ export class HeroService {
   }
 
   getHero(id: number): Promise<Hero> {
-    const  url = `${this.heroesUrl}/${id}`;
+    const url = `${this.heroesUrl}/${id}`;
     return this.http.get(url)
       .toPromise()
-      .then(response=>response.json().data as Hero)
+      .then(response => response.json().data as Hero)
       .catch(this.handleError);
   }
+
   private headers = new Headers({'Content-Type': 'application/json'});
 
-  update(hero: Hero):Promise<Hero>{
-    const  url = `${this.heroesUrl}/${hero.id}`;
-    return this.http.put(url,JSON.stringify(hero),{headers: this.headers})
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
       .toPromise()
-      .then(()=>hero)
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http.post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(result => result.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, this.headers)
+      .toPromise()
+      .then(() => null)
       .catch(this.handleError);
   }
 }
